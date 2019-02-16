c8c41a68578b524784222e5b0a1d0fbc84462cc4
Rodrigo Jose Sarmento Oliveira <rodrigojsoliveira@gmail.com>
2019-02-16 16:02:05 -0200

    Setup Google Maps API and show basic map view

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
+// Function used to initialize the map area.
+function initMap() {
+    // Since JQuery´s id selector returns a collection and the Map class
+    // receives an HTML element, we must use [0] to select the first element
+    // of the colletion.
+    var map = new google.maps.Map($("#map")[0],
+    {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
+
 // Application ViewModel
 var appViewModel = {
+    
 };
 
 // Activate knockout.js using JQuery, which will load Knockout after the DOM has finished loading.
