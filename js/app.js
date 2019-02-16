ec42b9c7df20eee70ce70e805ddcef1c3e6c7502
Rodrigo Jose Sarmento Oliveira <rodrigojsoliveira@gmail.com>
2019-02-16 14:25:10 -0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Application ViewModel
+var appViewModel = {
+};
+
+// Activate knockout.js using JQuery, which will load Knockout after the DOM has finished loading.
+$(function(){
+    ko.applyBindings(appViewModel);
+});
